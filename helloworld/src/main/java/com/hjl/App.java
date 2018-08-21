9cee33dcfdf929bd5e2f42e994272969fa6e3f35
hujianglai <hujianglai@yunzhangfang.com>
2018-08-21 12:00:30 +0800

    springboot deploy tomcat

--- helloworld/src/main/java/com/hjl/App.java
+++ helloworld/src/main/java/com/hjl/App.java
@@ -2,14 +2,21 @@ package com.hjl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 设置启动类,用于独立tomcat运行的入口
+        return builder.sources(App.class);
     }
 }
