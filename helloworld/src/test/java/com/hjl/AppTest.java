d4912117515a0dbc940623fc95031d1d2d24918b
hujianglai <hujianglai@yunzhangfang.com>
2018-10-13 11:35:44 +0800

    caipiao

--- helloworld/src/test/java/com/hjl/AppTest.java
+++ helloworld/src/test/java/com/hjl/AppTest.java
@@ -1,8 +1,9 @@
 package com.hjl;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Unit test for simple App.
@@ -12,9 +13,31 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public static void main(String[] args) {
+        //红球 33 选6
+        List<Integer> redBall = new ArrayList<Integer>();
+        for(int i=0;i<33;i++){
+            redBall.add(i+1);
+        }
+        System.out.println("开奖红球：" + select(redBall, 6));
+        //篮球16选1
+        List<Integer> blueBall = new ArrayList<Integer>();
+        for(int i=0;i<16;i++){
+            blueBall.add(i+1);
+        }
+        System.out.println("开奖蓝球：" + select(blueBall, 1));
+    }
+
+    public static List<Integer> select(List<Integer> list,int count){
+        List<Integer> selectedList = new ArrayList<Integer>();
+        Random random = new Random();
+        for(int i=0;i<count;i++){
+            int index = random.nextInt(list.size());
+            Integer number = list.get(index);
+            selectedList.add(number);
+            list.remove(index);
+        }
+        Collections.sort(selectedList);
+        return selectedList;
     }
 }
---
+++ produce/src/main/java/com/hjl/produce/mapper/DeclareConfigMapper.java
@@ -0,0 +1,16 @@
+package com.hjl.produce.mapper;
+
+import com.hjl.produce.dao.DeclareConfig;
+
+import java.util.List;
+
+/**
+ * Created   on 2018/10/8.
+ *
+ * @author hjl
+ */
+public interface DeclareConfigMapper {
+
+    List<DeclareConfig> selectAll();
+
+}
