662be1b792756fda9bf11f93fe4923a45b72716c
hujianglai <hujianglai@yunzhangfang.com>
2018-12-13 11:04:59 +0800

    读取配置文件

--- produce/src/main/java/com/hjl/produce/StartUp.java
+++ produce/src/main/java/com/hjl/produce/StartUp.java
@@ -4,7 +4,13 @@ import com.hjl.produce.contextinit.ContextInit3;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.retry.annotation.EnableRetry;
+
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created   on 2018/8/21.
@@ -20,7 +26,14 @@ public class StartUp {
         SpringApplication application = new SpringApplication(StartUp.class);
         application.addInitializers(new ContextInit3());
         application.run(args);
-
+        Resource resource = new ClassPathResource("test.properties");
+        Properties properties = null;
+        try {
+            properties = PropertiesLoaderUtils.loadProperties(resource);
+            System.out.println(properties.getProperty("name"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
